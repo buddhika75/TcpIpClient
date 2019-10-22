b9e3b32e606edd406fc36629401fbb31c0113476
chrishantha <chrishantha@BMS-Server>
2019-10-22 21:58:26 +0530

    Channel Count Increased

---
+++ src/tcpipclient/TcpIpClient.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tcpipclient;
+
+
+import java.net.*; 
+import java.io.*; 
+
+/**
+ *
+ * @author chrishantha
+ */
+public class TcpIpClient {
+//https://www.geeksforgeeks.org/socket-programming-in-java/
+    
+    // initialize socket and input output streams 
+    private Socket socket            = null; 
+    private DataInputStream  input   = null; 
+    private DataOutputStream out     = null; 
+
+    public TcpIpClient() {
+    }
+
+    
+    
+    
+    public TcpIpClient(String address, int port) {
+    
+        // establish a connection 
+        try
+        { 
+            socket = new Socket(address, port); 
+            System.out.println("Connected"); 
+  
+            // takes input from terminal 
+            input  = new DataInputStream(System.in); 
+  
+            // sends output to the socket 
+            out    = new DataOutputStream(socket.getOutputStream()); 
+        } 
+        catch(UnknownHostException u) 
+        { 
+            System.out.println(u); 
+        } 
+        catch(IOException i) 
+        { 
+            System.out.println(i); 
+        } 
+  
+        // string to read message from input 
+        String line = ""; 
+  
+        // keep reading until "Over" is input 
+        while (!line.equals("Over")) 
+        { 
+            try
+            { 
+                line = input.readLine(); 
+                out.writeUTF(line); 
+            } 
+            catch(IOException i) 
+            { 
+                System.out.println(i); 
+            } 
+        } 
+  
+        // close the connection 
+        try
+        { 
+            input.close(); 
+            out.close(); 
+            socket.close(); 
+        } 
+        catch(IOException i) 
+        { 
+            System.out.println(i); 
+        } 
+    
+    }
+    
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         TcpIpClient client = new TcpIpClient("127.0.0.1", 5000);
+    }
+    
+}
